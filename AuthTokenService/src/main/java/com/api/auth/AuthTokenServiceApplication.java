f11ddb30b79bbf00ce21766f48dd867ed14cae1b
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-10 10:43:45 -0500

    Creacion del servicio para auth

---
+++ AuthTokenService/src/main/java/com/api/auth/AuthTokenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTokenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTokenServiceApplication.class, args);
+	}
+
+}
