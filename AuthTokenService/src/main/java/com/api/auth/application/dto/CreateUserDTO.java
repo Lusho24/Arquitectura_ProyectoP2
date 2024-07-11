e06700903fc888172484af6a6f7d29e2ce096bf6
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-11 17:10:55 -0500

    fix script bdd y userDTO

--- AuthTokenService/src/main/java/com/api/auth/application/dto/CreateUserDTO.java
+++ AuthTokenService/src/main/java/com/api/auth/application/dto/CreateUserDTO.java
@@ -1,5 +1,9 @@
 package com.api.auth.application.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +15,31 @@ import java.util.Set;
 @NoArgsConstructor
 public class CreateUserDTO {
 
+    @NotBlank
+    @Size(max = 10)
     private String id;
+
+    @NotBlank
     private Integer idTienda;
+
+    @NotBlank
+    @Size(max = 32)
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
     private String address;
+
+    @NotBlank
     private String phone;
+
+    @NotNull
     private Set<String> roles;
 
 }
