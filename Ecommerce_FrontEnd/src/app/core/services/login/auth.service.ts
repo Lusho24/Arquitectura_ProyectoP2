7593c74c41eaf1ee73425179573a53fa68ab35e3
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-12 16:01:08 -0500

    test Lista de usuarios con tokens

--- Ecommerce_FrontEnd/src/app/core/services/login/auth.service.ts
+++ Ecommerce_FrontEnd/src/app/core/services/login/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -11,11 +11,17 @@ import { UserModel } from '../../models/userModel';
 export class AuthService {
 
   private urlEndPoint = `${environment.apiUrl}/users`;
+  
 
   constructor(private http:HttpClient) { }
 
   public findAll(): Observable<UserModel[]> {
-    return this.http.get<UserModel[]>(`${this.urlEndPoint}/all`);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJzdGFseW5mcmFuMDA3QGdtYWlsLmNvbSIsImlhdCI6MTcyMDgxNzMyNSwiZXhwIjoxNzIwOTAzNzI1fQ.P-xWRkZnvWxC1rimJpFljQC9ivePnske4i9v030Bz9Mr7iEKjREOuRUuvEYIjm0t`
+    });
+    
+    return this.http.get<UserModel[]>(`${this.urlEndPoint}/all`, { headers });
   }
 
 }
--- Ecommerce_FrontEnd/src/app/features/auth/login/login.component.ts
+++ Ecommerce_FrontEnd/src/app/features/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { RegisterComponent } from '../register/register.component';
+import { AuthService } from 'src/app/core/services/login/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +12,30 @@ import { RegisterComponent } from '../register/register.component';
 export class LoginComponent {
   signInForm: FormGroup;
 
-  constructor(public dialog: MatDialog,
-    private fb: FormBuilder) {
+  constructor(
+    public dialog: MatDialog,
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
     this.signInForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
-    openSignUp(): void {
+
+  openSignUp(): void {
     const dialogRef = this.dialog.open(RegisterComponent);
+  }
+
+  testUsers(): void {
+    console.log("HOLA MUNSO")
+    this.authService.findAll().subscribe({
+      next: (respose) =>{
+        console.log("RESPUESTA: ", respose)
+      },
+      error: (error) =>{
+        console.log("ERROR: ", error)
+      }
+    });
   }
 }
