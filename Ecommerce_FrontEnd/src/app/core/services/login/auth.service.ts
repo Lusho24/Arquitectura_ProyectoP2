8af97606f6cf72b7547a015f528393f2c858b659
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-13 00:40:08 -0500

    test auth service

--- Ecommerce_FrontEnd/src/app/core/services/login/auth.service.ts
+++ Ecommerce_FrontEnd/src/app/core/services/login/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { UserModel } from '../../models/userModel';
 
@@ -10,12 +10,30 @@ import { UserModel } from '../../models/userModel';
 export class AuthService {
 
   private urlEndPoint = environment.authUrl;
-  
 
-  constructor(private http:HttpClient) { }
 
+  constructor(private http: HttpClient) { }
+
+  // Método para iniciar sesion
   public login(user: UserModel): Observable<any> {
-    return this.http.post<any>(this.urlEndPoint, user);
+    return this.http.post<any>(this.urlEndPoint, user)
+      .pipe(
+        tap(response => {
+          localStorage.setItem('jwt', response.token);
+        })
+      );
   }
+
+  // Método para obtener el token
+  getToken() {
+    return localStorage.getItem('jwt');
+  }
+
+
+  // Método para cerrar sesión
+  public logout(): void {
+    localStorage.removeItem('jwt');
+  }
+
 
 }
