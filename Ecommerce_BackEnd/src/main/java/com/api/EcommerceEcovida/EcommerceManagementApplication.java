73f4b92f8eb95903ea20681daf50200f60fe81de
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-13 01:17:59 -0500

    Creacion del backend para ecovida

---
+++ Ecommerce_BackEnd/src/main/java/com/api/EcommerceEcovida/EcommerceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.api.EcommerceEcovida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceManagementApplication.class, args);
+	}
+
+}
---
+++ Ecommerce_BackEnd/src/test/java/com/api/EcommerceEcovida/EcommerceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.EcommerceEcovida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
