fbb8ec8969d7ee8214f242075a22413ee0ff5138
Camila Estefanía Naspud Cajamarca <93287477+CamilaNaspud@users.noreply.github.com>
2024-07-14 22:32:31 -0500

    admin agregar

--- Ecommerce_FrontEnd/src/app/features/admin/add-product/add-product.component.ts
+++ Ecommerce_FrontEnd/src/app/features/admin/add-product/add-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -9,5 +10,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddProductComponent 
 {
+
+  addProductForm: FormGroup;
+  selectedFile: File | null = null;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.addProductForm = this.fb.group({
+      name: ['', Validators.required],
+      price: [0, Validators.required],
+      description: ['', Validators.required],
+      stock: [0, Validators.required],
+      image: [null]
+    });
+  }
+
+  onAddClick(): void {
+    if (this.addProductForm.valid) {
+      const formData = this.addProductForm.value;
+
+      if (this.selectedFile) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          formData.image = e.target.result;
+          // Aquí puedes hacer una solicitud para guardar el producto, redirigir, etc.
+          console.log(formData);
+          this.router.navigate(['/admin/products']);
+        };
+        reader.readAsDataURL(this.selectedFile);
+      } else {
+        // Aquí puedes hacer una solicitud para guardar el producto, redirigir, etc.
+        console.log(formData);
+        this.router.navigate(['/admin/products']);
+      }
+    }
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onCancelClick(): void {
+    this.router.navigate(['/admin/products']);
+  }
   
 }
