d2b2e91958f2ee66b11cf19d1f50b2d6366d59e2
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-17 10:18:19 -0500

    Creacion del servicio y modelo de ORDEN-COMPRA/ORDEN-PEDIDO

---
+++ Ecommerce_FrontEnd/src/app/core/models/ecommerce/purchaseOrder.ts
@@ -0,0 +1,6 @@
+export interface PurchaseOrderModel {
+    id?: number;
+    paymentOrderId?: number;
+    creationDate?: Date;
+    state?: string;
+}
---
+++ Ecommerce_FrontEnd/src/app/core/services/ecommerce/purchase-order.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { PurchaseOrderModel } from '../../models/ecommerce/purchaseOrder';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PurchaseOrderService {
+
+  private urlEndPoint = `${environment.ecommerceUrl}/purchase-order`;
+
+  constructor(private http: HttpClient) { }
+
+  public findAll(): Observable<PurchaseOrderModel[]> {
+    return this.http.get<PurchaseOrderModel[]>(`${this.urlEndPoint}/all`);
+  }
+
+  public findById(id: number): Observable<PurchaseOrderModel> {
+    return this.http.get<PurchaseOrderModel>(`${this.urlEndPoint}/${id}`);
+  }
+
+  public save(purchaseOrder: PurchaseOrderModel): Observable<any> {
+    return this.http.post<any>(`${this.urlEndPoint}/save`, purchaseOrder);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.urlEndPoint}/delete/${id}`);
+  }
+
+}
