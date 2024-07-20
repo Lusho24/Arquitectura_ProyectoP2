9eff10a7c332c6348766427a27ef2144057582fc
Ricardogm2000 <rsgrijalva@espe.edu.ec>
2024-07-20 18:35:26 -0500

    Correccion Puertos

--- Ecommerce_FrontEnd/src/environments/environment.ts
+++ Ecommerce_FrontEnd/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    userUrl: 'http://localhost:4430/api',
-    authUrl: 'http://localhost:4430/login',
+    userUrl: 'http://localhost:443/api',
+    authUrl: 'http://localhost:443/login',
     ecommerceUrl: 'http://localhost:3000/api/ecommerce',
     apiUrl: 'http://localhost:puerto/ProductoService.asmx',
 
