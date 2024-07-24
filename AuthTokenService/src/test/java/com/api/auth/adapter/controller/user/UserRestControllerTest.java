1beaab176811ce1d57c79327139ed233f111f96c
Francisco-Suntaxi07 <stalynfran@hotmail.com>
2024-07-24 00:15:34 -0500

    Pruebas de integracion

--- AuthTokenService/src/test/java/com/api/auth/adapter/controller/user/UserRestControllerTest.java
+++ AuthTokenService/src/test/java/com/api/auth/adapter/controller/user/UserRestControllerTest.java
@@ -1,57 +1,104 @@
 package com.api.auth.adapter.controller.user;
 
+import com.api.auth.application.dto.CreateUserDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @WebAppConfiguration
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserRestControllerTest {
-    private final static String url = "/api/users";
-    private final static String token = "Bearer " +
-            "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiQ2FsbGUgUHJpbmNpcGFsIDQ1NiwgU2FuZ29scXVpIiwicGhvbmUiOiIwOTk4NzY1NDMyMiIsInJvbGVzIjpbeyJpZCI6MSwibmFtZSI6IkFETUlOIn0seyJpZCI6MiwibmFtZSI6IlVTRVIifV0sIm5hbWUiOiJBZG1pbmlzdHJhZG9yIiwiaWQiOiIxNzEyMzQ1Njc4IiwiaWF0IjoxNzIxNzg4Mzk5LCJleHAiOjE3MjE4NzQ3OTl9.kuua_3yDVWu0L9jhDa4AIlmxUBnpLDsjz7RwCC8NgP97qAC9wrWrqA0XdT7xRaoi";
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private ObjectMapper objectMapper;
+
+    private final static String url = "/api/users";
+
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
+    }
+
+    // * Pruebas con Resultado Exitoso
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    void findAllUsers_Success() throws Exception {
+        mockMvc.perform(get(url + "/all"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
     }
 
     @Test
-    void findAllUsers() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(url.concat("/all"))
-                        .header("Authorization", token)
-                        .accept(MediaType.APPLICATION_JSON_VALUE))
-                .andReturn();
+    @WithMockUser(roles = {"ADMIN"})
+    void findUserById_Success() throws Exception {
+        String userId = "1798765430";
 
-        assertEquals(200, result.getResponse().getStatus());
+        mockMvc.perform(get(url + "/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId));
     }
 
     @Test
-    void findUserById() {
+    void saveUser_Success() throws Exception {
+        CreateUserDTO user = new CreateUserDTO();
+        user.setId("17");
+        user.setIdTienda(1);
+        user.setName("Usuario Test");
+        user.setEmail("testuser@example.com");
+        user.setPassword("123456");
+        user.setAddress("Test direccion");
+        user.setPhone("098765");
+        user.setRoles(Set.of("USER"));
+
+        mockMvc.perform(post(url + "/save")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("17"));
     }
 
     @Test
-    void saveUser() {
+    @WithMockUser(roles = {"ADMIN"})
+    void deleteUserById_Success() throws Exception {
+        String userId = "17";
+
+        mockMvc.perform(delete(url + "/delete/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Usuario eliminado exitosamente."));
     }
 
+
+    // * Pruebas con Resultado Fallido
     @Test
-    void deleteUserById() {
+    void saveUserValidationErrors() throws Exception {
+        CreateUserDTO userDTO = new CreateUserDTO();
+
+        mockMvc.perform(post(url+"/save")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$", hasSize(8)));
     }
+
 }
