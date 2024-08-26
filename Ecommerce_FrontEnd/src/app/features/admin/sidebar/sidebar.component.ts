5b4d2599e628d3a575c30520cb5d2e07819856c7
Luis Xavier Espinosa Meza <lxespinosa@espe.edu.ec>
2024-08-26 16:26:46 -0500

    nombre de la tienda en sidebar

--- Ecommerce_FrontEnd/src/app/features/admin/sidebar/sidebar.component.ts
+++ Ecommerce_FrontEnd/src/app/features/admin/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import { SidebarService } from './sidebar.service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/login/auth.service';
 import { UserModel } from 'src/app/core/models/login/userModel';
+import { StoreService } from 'src/app/core/services/ecommerce/store.service';
 
 interface Menu {
   title: string;
@@ -34,17 +35,30 @@ export class SidebarComponent implements OnInit {
   [x: string]: any;
   menus: Menu[] = [];
   username: String = "";
+  storeName: string = '';
 
   constructor(
     private router: Router,
     public sidebarService: SidebarService,
-    private authService: AuthService
+    private authService: AuthService,
+    private storeService: StoreService 
   ) { }
 
   ngOnInit() {
     this.menus = this.sidebarService.getMenuList();
     const auxUser: UserModel = this.authService.getCurrentUser()!;
     this.username = auxUser.name!;
+  
+    // Obtener el nombre de la tienda usando el idTienda del usuario autenticado
+    this.storeService.findById(auxUser.idTienda!).subscribe(
+      store => {
+        this.storeName = store.name || 'Nombre de Tienda no disponible'; // Asignar valor por defecto
+      },
+      error => {
+        console.error('Error al obtener la tienda:', error);
+        this.storeName = 'Nombre de Tienda no disponible';
+      }
+    );
   }
 
   getSideBarState() {
