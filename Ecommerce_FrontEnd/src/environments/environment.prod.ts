19d9ca35064eeb5536d451f05a5f8c3da12baf1c
Francisco-Suntaxi07 <stalynfran@hotmail.com>
2024-08-28 01:46:41 -0500

    configuracion del frontend para la nube

--- Ecommerce_FrontEnd/src/environments/environment.prod.ts
+++ Ecommerce_FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-    production: true,
+    userUrl: 'https://auth-token-service-api-lg37rfueiq-rj.a.run.app/api',
+    authUrl: 'https://auth-token-service-api-lg37rfueiq-rj.a.run.app/login',
+    ecommerceUrl: 'https://ecommerce-core-api-lg37rfueiq-rj.a.run.app/api/ecommerce',
+    apiUrl: 'http://localhost:puerto/ProductoService.asmx',
+
+    production: true
 };
